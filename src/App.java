1829c3abf2f9a94a5d5e42306867f132cf0db97f
ginocanoy360 <amk1004232@student.hamk.fi>
2025-10-03 14:58:58 +0300

    Fixed the Indent
    minor changes on the indent and added some (********) characters

--- src/App.java
+++ src/App.java
@@ -7,12 +7,16 @@ public class App {
         Scanner in = new Scanner(System.in);
         Random rand = new Random();
 
-
-        System.out.println("\nEnter the money you have in 'Euro': ");
+        System.out.println("\n************************************");
+        System.out.println("Enter the money you have in 'Euro': ");
+        System.out.println("************************************");
         int money =Integer.parseInt(in.nextLine());
 
         while (money > 0) {
-            System.out.println("\nAre you ready to play? (yes/no): ");
+            System.out.println("\n********************************");
+            System.out.println("Are you ready to play? (yes/no): ");
+            System.out.println("********************************");
+
             String inputYesNo = in.nextLine().toLowerCase();
 
             // Conditional Statement asking the player to play the game
@@ -33,7 +37,7 @@ public class App {
             int number_3 = rand.nextInt(10) + 1;
 
             // Printing the random numbers
-            System.out.println("Here are your numbers:");
+            System.out.println("\nHere are your numbers:");
 	        System.out.println(number_1 + " " + number_2 + " " + number_3);
 
            
@@ -45,19 +49,19 @@ public class App {
             // Conditional Statements upon winning the game
 
             if (enteredNumber == 1) {
-                System.out.println("You won! You've guessed one (7)! Total earnings, 3 euro");
+                System.out.println("\nYou won! You've guessed one (7)! Total earnings, 3 euro");
                 money = money + 3;
             } else if (enteredNumber == 2) {
-                System.out.println("You won! You've guessed two (7s)! Total earnings, 5 euro");
+                System.out.println("\nYou won! You've guessed two (7s)! Total earnings, 5 euro");
                 money = money + 5;
             } else if (enteredNumber == 3) {
-                System.out.println("You won! You've guessed three (7s)! Total earnings, 10 euro");
+                System.out.println("\nYou won! You've guessed three (7s)! Total earnings, 10 euro");
                 money = money + 10;
             } else {
-                System.out.println("Sorry, you lost the game!");
+                System.out.println("\nSorry, you lost the game!");
             }
 
-            System.out.println("Your total earnings: " + money + " euro");
+            System.out.println("\nYour total earnings: " + money + " euro");
         }
 
         if (money <= 0) {
